556f4399651fca1121ae5f7cfd5d0ef8d8c4c2cd
Griffonyx <t.b_2000@hotmail.com>
2018-04-09 11:44:53 -0400

    Ordinateur (Portable et Bureau) Appelle des variables. Et mises à jour des modules.

--- ProjetFinal/Models/OrdiBureau.cs
+++ ProjetFinal/Models/OrdiBureau.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace ProjetFinal.Models
 {
-    public class OrdiBureau
+    public class OrdiBureau : Ordinateur
     {
+        //Identification de variables
+        public enum Boitier
+        {
+            Petit, Moyen, Gros
+        }
+        public enum CarteMere
+        {
+            microATX, miniATX, Moyen, Gros
+        }
     }
 }
--- ProjetFinal/Models/Ordinateur.cs
+++ ProjetFinal/Models/Ordinateur.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace ProjetFinal.Models
 {
-    public class Ordinateur
+    public class Ordinateur : Item
     {
+        //Recherche des variables
+        [Required]
+        public int memoire { get; set; }
+        [Required]
+        public float processeur { get; set; }
+        [Required]
+        public String carteGraphique { get; set; }
 
+        
     }
 }
