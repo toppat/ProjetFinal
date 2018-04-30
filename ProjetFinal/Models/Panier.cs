231c7a4db5292e1537937bd34571e1c95937c21c
Pat <9219221@CegepGranby.qc.ca>
2018-04-30 10:55:54 -0400

    prendre des note dans Panier

--- ProjetFinal/Models/Panier.cs
+++ ProjetFinal/Models/Panier.cs
@@ -21,11 +21,22 @@ namespace ProjetFinal.Models
 
 
 
-//PanierItems i = List.find(NetPipeStyleUriParser.item.id==itemID)
+//PanierItems i = List.find(Pi=>Pi.item.id==itemId)
     //if (i==null)
     //new PanierItem(......)
 
 //@html.DropDownList("Nom", SelectList, "Selectionner un client", HtmlAttributes)
 // new SelectList(ListeClients, "id", "Nom")
 
+//1.obtenir la liste des clients                  dans Controller items
+//2.Passer la liste de clients a la vue (ViewBag) dans Controller items
 
+//3.Créer un DropDownList dans la vue avec la liste de client  dans Vue (Details)
+//4.Appeler un Action"Ajouter"dans le controller de panier     dans Vue (Details)
+
+//5.Aller chercher la valeur selectionnéedu DropDownList       dans controller Panier
+//6.Aller chercher le panier du client                         dans controller Panier
+//7.Ajouter le PanierItem dans la liste du panier              dans controller Panier
+    //- Juste modifier la quantité si existe déja
+    //- si non en créer un nouveau
+//8.db.saveChange()
